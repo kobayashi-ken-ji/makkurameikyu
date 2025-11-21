e99bb49b492a07b3357bceba88d6a8659146b102
kobayashi <konata.t@gmail.com>
2025-11-21 09:53:53 +0900

    feat: Hello Wourld 環境を構築

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+class Main {
+    greet() {
+        const msg = 'Hello, TypeScript';
+        const msg2 = "こんにちは";
+        console.log(msg, msg2);
+    }
+}
+new Main().greet();
+export {};
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+class Main
+{
+    greet() {
+        const msg:string = 'Hello, TypeScript';
+        const msg2:string = "こんにちは";
+        console.log(msg, msg2);
+    }
+}
+
+new Main().greet();
